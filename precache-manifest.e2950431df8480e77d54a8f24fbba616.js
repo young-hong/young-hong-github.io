912b67eb64e42ef46ecde9351d76f564bd6851db
hongqiqiang <1914588872@qq.com>
2020-11-11 14:41:02 +0800

    init booking

---
+++ precache-manifest.e2950431df8480e77d54a8f24fbba616.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ebde8b07bdd3c4790affc6c1455c6e1",
+    "url": "/booking/index.html"
+  },
+  {
+    "revision": "cb4c7ec83aa0167ec13a",
+    "url": "/booking/static/css/2.ab3073af.chunk.css"
+  },
+  {
+    "revision": "cb4c7ec83aa0167ec13a",
+    "url": "/booking/static/js/2.46326aa0.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/booking/static/js/2.46326aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99ae1e1b8aa3952d8f18",
+    "url": "/booking/static/js/main.c2eaf656.chunk.js"
+  },
+  {
+    "revision": "7ef55738f25a6eaa386b",
+    "url": "/booking/static/js/runtime-main.729334de.js"
+  }
+]);
